fbea3108ae27e73a28e4cac3a5cd4f56cbc3455c
Peter Regan <peter.mitchell.regan@gmail.com>
2015-11-01 13:11:56 -0700

    fuck me

--- Project2/src/Gui.cpp
+++ Project2/src/Gui.cpp
@@ -1,6 +1,7 @@
 #include "main.hpp"
 
 static const int PANEL_HEIGHT=7;
+static const int MSG_HEIGHT=PANEL_HEIGHT-1;
 
 Gui::Gui(int yOffset):yOffset(yOffset)
 {
@@ -9,6 +10,7 @@ Gui::Gui(int yOffset):yOffset(yOffset)
 Gui::~Gui()
 {
 	delete con;
+	log.clearAndDelete();
 }
 
 void Gui::render()
@@ -19,6 +21,14 @@ void Gui::render()
 
 	//blit the GUI console on the root console
 	TCODConsole::blit(con,0,0,engine.screenWidth,PANEL_HEIGHT,TCODConsole::root,0,engine.screenHeight-PANELHEIGHT-yOffset)
+
+	//draw the message log
+	int y=1;
+	for (Message **i=log.begin(); i!=log.end(); i++)
+	{
+		Message *message=*i;
+		con->
+	}
 }
 
 
